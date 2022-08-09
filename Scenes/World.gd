434aa9eb74cc7d3f8d3e6dcfe584ede73a5cfd44
waltyy <100096985+waltyy@users.noreply.github.com>
2022-08-09 12:23:36 +1200

    Timer Done

--- Scenes/World.gd
+++ Scenes/World.gd
@@ -6,4 +6,6 @@ func _ready():
 func _process(delta):
 	pass
 	if Input.is_action_just_pressed("reset"):
-		pass
+		GameStats.reset()
+	print(GameStats.get_time())
+	$CanvasLayer/Control/Label.text = GameStats.get_time()
