d0ad4e57b2adcda8ce06fde0b82f29bae4f8eeac
waltyy <100096985+waltyy@users.noreply.github.com>
2022-08-10 13:14:48 +1200

    Fixed Some Bugs

--- Scenes/Camera2D.gd
+++ Scenes/Camera2D.gd
@@ -1,26 +1,30 @@
 extends Camera2D
 
 var target = null
-var zoomed = false
+var zoomed = true
 var center = Vector2.ZERO
 
 
 func _ready():
 	center = get_viewport_rect().size/2
+	target = owner.get_node("Player")
+	#
+	position = target.global_position
+	zoom = Vector2(.3,.3)
 	
 func _process(delta):
 	if Input.is_action_just_pressed("zoom"):
 		if zoomed:
-			target = center
+			target = null
 			zoomed = false
 		else:
 			target = owner.get_node("Player")
 			zoomed = true
-			
+	
 	if zoomed:
-		zoom = zoom.move_toward(Vector2(0.3,0.3), 0.03)
-		position = position.move_toward(target.global_position,80)
+		position = target.global_position
+		zoom = Vector2(.3,.3)
 		
 	else:
-		zoom = zoom.move_toward(Vector2(1,1),0.03)
-		position = position.move_toward(center,80)
+		position = center
+		zoom = Vector2(1,1)
